756b765a2073875a3394f1d6238ef9657faeda65
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-08-16 21:55:05 +0200

    🎨 Format code

--- src/V1/Exchange.sol
+++ src/V1/Exchange.sol
@@ -4,17 +4,21 @@ pragma solidity 0.8.20;
 import {IERC20} from "openzeppelin/token/ERC20/IERC20.sol";
 
 /**
-* @notice In UniswapV1 it is only possible to exchange 1 type of token for ETH. 
-*/
+ * @notice In UniswapV1 it is only possible to exchange 1 type of token for ETH.
+ */
 contract Exchange {
-   address public tokenAddress;
+    address public tokenAddress;
 
-   constructor (address _tokenAddress) {
-       require(_tokenAddress != address(0), "Token address cannot be 0");
-       tokenAddress = _tokenAddress;
-   }
+    constructor(address _tokenAddress) {
+        require(_tokenAddress != address(0), "Token address cannot be 0");
+        tokenAddress = _tokenAddress;
+    }
 
-   function addLiquidity(uint256 _amount) public payable {
-       IERC20(tokenAddress).transferFrom(msg.sender, address(this), _amount);
-   }
+    /**
+     * @notice Provide _amount of tokens to the exchange contract as liquidity.
+     * Before calling this function make sure to approve the exchange contract.
+     */
+    function addLiquidity(uint256 _amount) public payable {
+        IERC20(tokenAddress).transferFrom(msg.sender, address(this), _amount);
+    }
 }
--- src/V1/Token.sol
+++ src/V1/Token.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.20;
 import {ERC20} from "openzeppelin/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
-    constructor (string memory _name, string memory _symbol, uint256 _initialSupply) ERC20 (_name, _symbol) {
+    constructor(string memory _name, string memory _symbol, uint256 _initialSupply) ERC20(_name, _symbol) {
         _mint(msg.sender, _initialSupply);
     }
 }

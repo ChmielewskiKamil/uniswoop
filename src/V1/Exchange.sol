fcebf09ea064fda9c0e0c1b5defde28f05588acb
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-08-18 20:52:18 +0200

    ✨ Add getEthAmount function based on tokens

--- src/V1/Exchange.sol
+++ src/V1/Exchange.sol
@@ -25,7 +25,7 @@ contract Exchange {
      * @notice Provide _amount of tokens to the exchange contract as liquidity.
      * Before calling this function make sure to approve the exchange contract.
      *
-     * @dev This function is payable to allow native tokens
+     * @dev This function is PAYABLE to allow native tokens
      * to be added as liquidity as well.
      */
     function addLiquidity(uint256 _amount) public payable {
@@ -33,6 +33,7 @@ contract Exchange {
     }
 
     /*~*~*~*~*~*~*~*~*~*~*~*~* View Functions ~*~*~*~*~*~*~*~*~*~*~*~*~/
+
     /**
      * @notice Get the amount of tokens held in the exchange contract.
      * These tokens are available for trading.
@@ -41,14 +42,42 @@ contract Exchange {
         return IERC20(tokenAddress).balanceOf(address(this));
     }
 
+    /**
+     * @notice Calculates the amount of ERC20 tokens that would be received,
+     * in exchange for _ethInputAmount of ETH. BE AWARE OF THE SLIPPAGE!
+     */
+    function getTokenAmount(uint256 _ethInputAmount) public view returns (uint256) {
+        require(_ethInputAmount > 0, "Input amount has to be > 0");
+
+        uint256 tokenReserves = getReserves();
+
+        return _getAmount(_ethInputAmount, address(this).balance, tokenReserves);
+    }
+
+    /* @audit-issue This does not consider diff in decimals? */
+    /**
+     * @notice Calculates the amount of ETH that would be received,
+     * in exchange for _tokenInputAmount of ERC20 tokens. BE AWARE OF THE SLIPPAGE!
+     */
+    function getEthAmount(uint256 _tokenInputAmount) public view returns (uint256) {
+        require(_tokenInputAmount > 0, "Input amount has to be > 0");
+
+        uint256 tokenReserves = getReserves();
+
+        return _getAmount(_tokenInputAmount, tokenReserves, address(this).balance);
+    }
+
     ////////////////////////////////////////////////////////////////////
     //                            Private                             //
     ////////////////////////////////////////////////////////////////////
+
     /**
-    * @param _inputAmount Amount of token 'x' to be exchanged for token 'y'
-    * @param _inputReserves Amount of token 'x' held in the exchange contract
-    * @param _outputReserves Amount of token 'y' held in the exchange contract
-    */
+     * @dev In case of UniswapV1 one of the tokens is always ETH.
+     * @param _inputAmount Amount of token 'x' to be exchanged for token 'y'
+     * @param _inputReserves Amount of token 'x' held in the exchange contract
+     * @param _outputReserves Amount of token 'y' held in the exchange contract
+     * @return Amount of token 'y' to be received in exchange for token 'x'
+     */
     function _getAmount(uint256 _inputAmount, uint256 _inputReserves, uint256 _outputReserves)
         private
         pure
